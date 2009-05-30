7c001c49ed6163f0232798073efdd35a23c6e05b
aartemenko <aartemenko@4f0b9867-19ab-4b67-877a-764fa0eeb145>
2009-05-30 08:15:47 +0000

    Added empty django_antichaos app with setup.py and MANIFEST.in.
    git-svn-id: svn://alt.djangodash.com/18_antihaos@5 4f0b9867-19ab-4b67-877a-764fa0eeb145

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+from django_antichaos import __version__ as version
+
+setup(
+    name = 'django-antichaos',
+    version = version,
+    description = """Django application for easy tag manipulation."""
+                  """It's addon to django-tagging or django-taggin-ng applications.""",
+    keywords = 'django apps tagging tags jquery javascript',
+    license = 'New BSD License',
+    author = 'Alexander Artemenko',
+    author_email = 'svetlyak.40wt@gmail.com',
+    url = 'http://github.com/svetlyak40wt/django-antichaos/',
+    install_requires = [],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Plugins',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Javascript',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    package_dir = {'': 'src'},
+    packages = find_packages('src'),
+    include_package_data = True,
+    zip_safe = False,
+)
+
