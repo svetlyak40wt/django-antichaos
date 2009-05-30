04244b8be20422235fa4bddb0d7669d34f4136ea
aartemenko <aartemenko@4f0b9867-19ab-4b67-877a-764fa0eeb145>
2009-05-30 13:42:35 +0000

    Added unittest for getting models with tags.
    git-svn-id: svn://alt.djangodash.com/18_antihaos@21 4f0b9867-19ab-4b67-877a-764fa0eeb145

--- src/django_antichaos/tests.py
+++ src/django_antichaos/tests.py
@@ -13,3 +13,15 @@ class TagCloudTests(TestCase):
         self.assertEqual(0, Post.objects.count())
         Post(tags = 'one, two, three').save()
         self.assertEqual(1, Post.objects.count())
+
+    def testContentTypes(self):
+        from django_antichaos import get_tagged_models
+
+        Post(tags = 'one, two, three').save()
+        Link(tags = 'six, seven').save()
+
+        mods = get_tagged_models()
+        self.assertEqual(2, len(mods))
+        self.assert_(Post in mods)
+        self.assert_(Link in mods)
+
