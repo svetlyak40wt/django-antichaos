d2fcbd70cafc2774937053b782e0384116f740a5
aartemenko <aartemenko@4f0b9867-19ab-4b67-877a-764fa0eeb145>
2009-05-30 20:59:03 +0000

    Added some fancy animation on tag drop.
    git-svn-id: svn://alt.djangodash.com/18_antihaos@44 4f0b9867-19ab-4b67-877a-764fa0eeb145

--- src/django_antichaos/media/antichaos.js
+++ src/django_antichaos/media/antichaos.js
@@ -18,6 +18,19 @@ $(document).ready(function() {
             history.append(
                 $('<li>' + from.html() + '->' + to.html() + '</li>')
             );
+            var from_count = eval(from.find('sup').html());
+            var from_size = from.css('font-size');
+            from_size = eval(from_size.substring(0, from_size.length - 2));
+
+            var to_count   = eval(to.find('sup').html());
+            var to_size = to.css('font-size');
+            to_size = eval(to_size.substring(0, to_size.length - 2));
+
+            to.find('sup').html(to_count + from_count);
+            to.animate({
+                fontSize: to_size + from_size,
+            });
+
             ui.draggable.effect('explode');
         },
     });
