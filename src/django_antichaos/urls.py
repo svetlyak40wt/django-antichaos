90a638131b3505e0e78facb0e5985d6e51ea4ad3
aartemenko <aartemenko@4f0b9867-19ab-4b67-877a-764fa0eeb145>
2009-05-30 16:33:44 +0000

    Automatic media link to MEDIA_ROOT.
    git-svn-id: svn://alt.djangodash.com/18_antihaos@33 4f0b9867-19ab-4b67-877a-764fa0eeb145

--- src/django_antichaos/urls.py
+++ src/django_antichaos/urls.py
@@ -1,6 +1,24 @@
+import logging
+import os.path
+
+from django.conf import settings
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('django_antichaos.views',
     (r'^cloud/(?P<ctype_id>\d+)/$', 'cloud', {}, 'tag-cloud'),
 )
 
+# create link in media directory
+if not os.path.exists(settings.MEDIA_ROOT):
+    os.makedirs(settings.MEDIA_ROOT)
+
+APP_MEDIA_ROOT = os.path.join(
+    settings.MEDIA_ROOT,
+    'antichaos')
+
+if not os.path.exists(APP_MEDIA_ROOT):
+    _local = os.path.join(
+                os.path.dirname(__file__),
+                'media')
+    logging.info('linking %r to %r' % (_local, APP_MEDIA_ROOT))
+    os.symlink(_local, APP_MEDIA_ROOT)
