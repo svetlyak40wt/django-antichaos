6221869f4a8cbe59cb44d556b246b61c8011a87c
aartemenko <aartemenko@4f0b9867-19ab-4b67-877a-764fa0eeb145>
2009-05-30 20:58:58 +0000

    Added function to act on 'drop', plus history log.
    git-svn-id: svn://alt.djangodash.com/18_antihaos@42 4f0b9867-19ab-4b67-877a-764fa0eeb145

--- src/django_antichaos/media/antichaos.js
+++ src/django_antichaos/media/antichaos.js
@@ -1,7 +1,22 @@
+var stack = [];
+
 $(document).ready(function() {
+    var history = $('.history');
+
     $('.tag').draggable().droppable({
         accept: '.tag',
         activeClass: 'active',
         hoverClass:  'hover',
+        drop: function(ev, ui) {
+            stack[stack.length] = {
+                action: 'merge',
+                from: ui.draggable[0].id,
+                to: $(this)[0].id
+            };
+            history.append(
+                $('<li>' + ui.draggable.html() + '->' + $(this).html() + '</li>')
+            );
+            ui.draggable.effect('explode');
+        },
     });
 });
