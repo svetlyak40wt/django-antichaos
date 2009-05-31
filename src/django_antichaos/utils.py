a01723215301279f03333a8414e2c270db0fbf89
aartemenko <aartemenko@4f0b9867-19ab-4b67-877a-764fa0eeb145>
2009-05-31 11:39:50 +0000

    Added methods to process changes.
    git-svn-id: svn://alt.djangodash.com/18_antihaos@53 4f0b9867-19ab-4b67-877a-764fa0eeb145

--- src/django_antichaos/utils.py
+++ src/django_antichaos/utils.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.contenttypes.models import ContentType
 from tagging.models import TaggedItem
 
@@ -6,8 +8,25 @@ def get_tagged_models():
     ctypes = ContentType._default_manager.filter(id__in = cids)
     return [(ctype.id, ctype.model_class()) for ctype in ctypes]
 
-def process_commands(ctype, commands):
-    pass
 
 def model_to_ctype(model):
     return ContentType._default_manager.get_for_model(model)
+
+def process_merge(ctype, tag_left, tag_right):
+    logger = logging.getLogger('antichaos.utils')
+    logger.debug('merging tag_id %s to tag_id %s' % (tag_right, tag_left))
+
+def process_commands(ctype, commands):
+    logger = logging.getLogger('antichaos.utils')
+    logger.debug('processing commands')
+
+    processors = dict((name.split('_', 1)[1], value)
+        for name, value in globals().iteritems()
+            if name.startswith('process_') and name != 'process_commands')
+
+    for cmd in commands:
+        cmd, args = cmd.split(' ', 1)
+        proc = processors.get(cmd, None)
+        if proc is not None:
+            proc(ctype, *args.split(' '))
+
